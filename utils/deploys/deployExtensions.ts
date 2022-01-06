d83299edd921243ac6a65807d82e5f2423341c6e
alpha-guy <alphaguy2019@gmail.com>
2022-01-06 13:00:44 -0800

    Add PerpV2LeverageStrategyExtension contract, tests

--- utils/deploys/deployExtensions.ts
+++ utils/deploys/deployExtensions.ts
@@ -1,11 +1,18 @@
 import { Signer, BigNumber } from "ethers";
 import {
-  Address
+  Address,
+  PerpV2ContractSettings,
+  PerpV2MethodologySettings,
+  PerpV2ExecutionSettings,
+  PerpV2IncentiveSettings,
+  PerpV2ExchangeSettings
 } from "../types";
 import {
+  PerpV2LeverageStrategyExtension,
   StreamingFeeSplitExtension
 } from "../contracts/index";
 
+import { PerpV2LeverageStrategyExtension__factory } from "../../typechain/factories/PerpV2LeverageStrategyExtension__factory";
 import { StreamingFeeSplitExtension__factory } from "../../typechain/factories/StreamingFeeSplitExtension__factory";
 
 export default class DeployExtensions {
@@ -13,6 +20,24 @@ export default class DeployExtensions {
 
   constructor(deployerSigner: Signer) {
     this._deployerSigner = deployerSigner;
+  }
+
+  public async deployPerpV2LeverageStrategyExtension(
+    manager: Address,
+    contractSettings: PerpV2ContractSettings,
+    methodologySettings: PerpV2MethodologySettings,
+    executionSettings: PerpV2ExecutionSettings,
+    incentiveSettings: PerpV2IncentiveSettings,
+    exchangeSettings: PerpV2ExchangeSettings
+  ): Promise<PerpV2LeverageStrategyExtension> {
+    return await new PerpV2LeverageStrategyExtension__factory(this._deployerSigner).deploy(
+      manager,
+      contractSettings,
+      methodologySettings,
+      executionSettings,
+      incentiveSettings,
+      exchangeSettings,
+    );
   }
 
   public async deployStreamingFeeSplitExtension(
