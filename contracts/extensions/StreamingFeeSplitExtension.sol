d976ff569f263970e75987d49b4cb4b09b2f37e2
bweick <brian.weickmann@gmail.com>
2022-03-02 10:57:09 -0800

    Moving extensions to this branch.

---
+++ contracts/extensions/StreamingFeeSplitExtension.sol
@@ -0,0 +1,193 @@
+/*
+    Copyright 2022 Set Labs Inc.
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    SPDX-License-Identifier: Apache License, Version 2.0
+*/
+
+pragma solidity 0.6.10;
+pragma experimental "ABIEncoderV2";
+
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
+import { PreciseUnitMath } from "@setprotocol/set-protocol-v2/contracts/lib/PreciseUnitMath.sol";
+
+import { BaseGlobalExtension } from "../lib/BaseGlobalExtension.sol";
+import { IDelegatedManager } from "../interfaces/IDelegatedManager.sol";
+import { ISetToken } from "../interfaces/ISetToken.sol";
+import { IStreamingFeeModule } from "../interfaces/IStreamingFeeModule.sol";
+
+/**
+ * @title StreamingFeeSplitExtension
+ * @author Set Protocol
+ *
+ * Smart contract global extension which provides DelegatedManager owner and 
+ * methodologist the ability to accrue and split streaming fees at an mutable percentage.
+ */
+contract StreamingFeeSplitExtension is BaseGlobalExtension {
+    using Address for address;
+    using PreciseUnitMath for uint256;
+    using SafeMath for uint256;
+
+    /* ============ Events ============ */
+
+    event ExtensionInitialized(
+        address _setToken,
+        address _delegatedManager
+    );
+
+    event FeesDistributed(
+        address _setToken,
+        address indexed _ownerFeeRecipient,
+        address indexed _methodologist,
+        uint256 _ownerTake,
+        uint256 _methodologistTake
+    );
+
+    /* ============ State Variables ============ */
+
+    // Instance of StreamingFeeModule
+    IStreamingFeeModule public immutable streamingFeeModule;
+
+    // Mapping from Set Token to DelegatedManager 
+    mapping(ISetToken => IDelegatedManager) public setManagers;
+
+    /* ============ Constructor ============ */
+
+    constructor(
+        IStreamingFeeModule _streamingFeeModule
+    )
+        public
+    {
+        streamingFeeModule = _streamingFeeModule;
+    }
+
+    /* ============ External Functions ============ */
+
+    /**
+     * ANYONE CALLABLE: Accrues fees from streaming fee module. Gets resulting balance after fee accrual, calculates fees for
+     * owner and methodologist, and sends to owner fee recipient and methodologist respectively.
+     */
+    function accrueFeesAndDistribute(ISetToken _setToken) public {
+        // Emits a FeeActualized event
+        streamingFeeModule.accrueFee(_setToken);
+
+        uint256 totalFees = _setToken.balanceOf(address(_manager(_setToken)));
+
+        address methodologist = _manager(_setToken).methodologist();
+        address ownerFeeRecipient = _manager(_setToken).ownerFeeRecipient();
+
+        uint256 ownerTake = totalFees.preciseMul(_manager(_setToken).ownerFeeSplit());
+        uint256 methodologistTake = totalFees.sub(ownerTake);
+
+        if (ownerTake > 0) {
+            _setToken.transfer(ownerFeeRecipient, ownerTake);
+        }
+
+        if (methodologistTake > 0) {
+            _setToken.transfer(methodologist, methodologistTake);
+        }
+
+        emit FeesDistributed(address(_setToken), ownerFeeRecipient, methodologist, ownerTake, methodologistTake);
+    }
+
+    /**
+     * ONLY OWNER: Initializes StreamingFeeSplitExtension to the DelegatedManager.
+     *
+     * @param _delegatedManager     Instance of the DelegatedManager to initialize
+     */
+    function initializeExtension(IDelegatedManager _delegatedManager) public {
+        require(msg.sender == _delegatedManager.owner(), "Must be owner");
+        require(_delegatedManager.isPendingExtension(address(this)), "Extension must be pending");
+
+        setManagers[_delegatedManager.setToken()] = _delegatedManager;
+
+        _delegatedManager.initializeExtension();
+
+        ExtensionInitialized(address(_delegatedManager.setToken()), address(_delegatedManager));
+    }
+
+    /**
+     * ONLY OWNER: Initializes StreamingFeeSplitExtension to the DelegatedManager and StreamingFeeModule to the SetToken
+     *
+     * @param _delegatedManager     Instance of the DelegatedManager to initialize
+     * @param _settings             FeeState struct defining fee parameters for StreamingFeeModule initialization
+     */
+    function initializeModuleAndExtension(
+        IDelegatedManager _delegatedManager,
+        IStreamingFeeModule.FeeState memory _settings
+    ) 
+        external 
+    {
+        require(msg.sender == _delegatedManager.owner(), "Must be owner");
+        require(_delegatedManager.setToken().isPendingModule(address(streamingFeeModule)), "StreamingFeeModule must be pending");
+        require(_delegatedManager.isPendingExtension(address(this)), "Extension must be pending");
+
+        initializeExtension(_delegatedManager);
+
+        _delegatedManager.initializeExtension();
+
+        streamingFeeModule.initialize(_delegatedManager.setToken(), _settings);
+
+        ExtensionInitialized(address(_delegatedManager.setToken()), address(_delegatedManager));
+    }
+
+    /**
+     * ONLY OWNER: Remove an existing SetToken and DelegatedManager tracked by the StreamingFeeSplitExtension 
+     *
+     * @param _setToken     Instance of the SetToken to remove
+     */
+    function removeExtension(ISetToken _setToken) external override onlyOwner(_setToken) {
+        require(address(setManagers[_setToken]) != address(0), "Must be existing Set Token");
+        delete setManagers[_setToken];
+    }
+
+    /**
+     * ONLY OWNER: Updates streaming fee on StreamingFeeModule.
+     *
+     * NOTE: This will accrue streaming fees though not send to owner fee recipient and methodologist.
+     *
+     * @param _setToken     Instance of the SetToken to update streaming fee for
+     * @param _newFee       Percent of Set accruing to fee extension annually (1% = 1e16, 100% = 1e18)
+     */
+    function updateStreamingFee(ISetToken _setToken, uint256 _newFee)
+        external
+        onlyOwner(_setToken)
+    {
+        bytes memory callData = abi.encodeWithSignature("updateStreamingFee(address,uint256)", _setToken, _newFee);
+        invokeManager(_setToken, address(streamingFeeModule), callData);
+    }
+
+    /**
+     * ONLY OWNER: Updates fee recipient on StreamingFeeModule
+     *
+     * @param _setToken         Instance of the SetToken to update fee recipient for
+     * @param _newFeeRecipient  Address of new fee recipient. This should be the address of the DelegatedManager
+     */
+    function updateFeeRecipient(ISetToken _setToken, address _newFeeRecipient)
+        external
+        onlyOwner(_setToken)
+    {
+        bytes memory callData = abi.encodeWithSignature("updateFeeRecipient(address,address)", _setToken, _newFeeRecipient);
+        invokeManager(_setToken, address(streamingFeeModule), callData);
+    }
+
+    /* ============ Internal Functions ============ */
+
+    /**
+     * Internal function to grab manager of passed SetToken from StreamingFeeSplitExtension data structure.
+     *
+     * @param _setToken         SetToken who's manager is needed 
+     */
+    function _manager(ISetToken _setToken) internal override view returns (IDelegatedManager) {
+        return setManagers[_setToken];
+    }
+ }
